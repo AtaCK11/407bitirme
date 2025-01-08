9ecc1c0934bf73ba7d89fc58fde3de0141e912ff
Atac <119973997+AtaCK11@users.noreply.github.com>
2025-01-09 02:31:53 +0300

    finalize the first part of the project
    + data transmission
    + fast implementation of encryption n such

--- bitirme407_patient.ino
+++ bitirme407_patient.ino
@@ -1,3 +1,15 @@
+
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Wire.h>
+#include <MAX30100_PulseOximeter.h>
+#include <MQUnifiedsensor.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <DHT.h>
+
 #define ECG_BUFFER_SIZE 256  // Buffer size for ECG
 #define HR_BUFFER_SIZE 5     // Buffer size for heart rate
 
@@ -5,8 +17,9 @@
 
 // Sample rates
 #define ECG_SAMPLE_RATE 1
+#define ECG_DATA_TX_RATE 200
 #define HR_SAMPLE_RATE 1
-#define HR_PUSH_SAMPLE_RATE 2000
+#define HR_PUSH_SAMPLE_RATE 500
 #define MQ135_SAMPLE_RATE 8000
 #define TEMP_SAMPLE_RATE 5000
 #define ROOM_SAMPLE_RATE 5000
@@ -20,18 +33,28 @@
 #define MQ135_PIN 35
 #define DS18B20_PIN 33
 #define DHT11_PIN 32
+#define BUZZER_PIN 5
+#define RGB_R_PIN 14
+#define RGB_G_PIN 12
+#define RGB_B_PIN 13
 
+ 
 // Defaults
 #define RatioMQ135CleanAir 3.6
 
 #define CUTOFF_FREQUENCY 0.5
 
-#include <Wire.h>
-#include <MAX30100_PulseOximeter.h>
-#include <MQUnifiedsensor.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <DHT.h>
+#define DEBUG
+
+#ifdef DEBUG
+  #define debugPrint(x) Serial.print(x)
+  #define debugPrintln(x) Serial.println(x)
+  #define debugPrintfHex(x) Serial.printf("%02X", x)
+#else
+  #define debugPrint(x)
+  #define debugPrintln(x)
+  #define debugPrintfHex(x)
+#endif
 
 enum SensorType {
   ECG,
@@ -116,9 +139,15 @@ QueueHandle_t sensorQueue;
 
 // SensorData structure
 struct SensorData {
-  const char *sensorType;
+  char sensorType[36];
   float value;
 };
+
+// esp receiver
+uint8_t receiverMACAddress[] = {0xA0, 0xB7, 0x65, 0x28, 0xCE, 0xA0};
+
+static const char* PMK_KEY_STR = "U@Kw%nV&PzbRK2WM";
+static const char* LMK_KEY_STR = "Ruth:WqH9@j%h5qg";
 
 // sensors
 PulseOximeter pox;
@@ -130,13 +159,66 @@ DallasTemperature sensors(&oneWire);
 DHT dht(DHT11_PIN, DHT11);
 
 
+void setRGBColor(int red, int green, int blue)
+{
+  debugPrintln("\n");
+  analogWrite(RGB_R_PIN, red);
+  analogWrite(RGB_G_PIN, green);
+  analogWrite(RGB_B_PIN, blue);
+}
+
+void onPacketSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  debugPrint("\nLast Packet:\t");
+  debugPrintln(status == ESP_NOW_SEND_SUCCESS ? "Sent" : "Fail");
+}
+
 void setup() {
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  delay(500);
+
+  pinMode(RGB_R_PIN, OUTPUT);
+  pinMode(RGB_G_PIN, OUTPUT);
+  pinMode(RGB_B_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  // Print the device's MAC address
+  debugPrint("Sender MAC Address: ");
+  debugPrintln(WiFi.macAddress());
+
+  // Initialize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    debugPrintln("Error initializing ESP-NOW");
+    setRGBColor(0, 255, 255); // RED
+    while (true);
+  }
+
+  delay(1000);
+  // ESP NOW
+  //esp_now_register_recv_cb(OnDataRecv);
+  esp_now_set_pmk((uint8_t *)PMK_KEY_STR);
+
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, receiverMACAddress, 6);
+  peerInfo.encrypt = true;
+  for (uint8_t i = 0; i < 16; i++) {
+    peerInfo.lmk[i] = LMK_KEY_STR[i];
+  }
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    debugPrintln("Failed to add peer");
+    setRGBColor(0, 255, 255); // RED
+    while (true);
+  }
+
+  esp_now_register_send_cb(onPacketSent);
 
   // RTOS queue
   sensorQueue = xQueueCreate(QUEUE_SIZE, sizeof(SensorData));
   if (sensorQueue == NULL) {
-    Serial.println("Error creating the queue");
+    debugPrintln("Error creating the queue");
+    setRGBColor(0, 255, 255); // RED
     while (true);
   }
   delay(100);
@@ -151,9 +233,11 @@ void setup() {
   {
     for (int i = 0; i < 10; i++) 
     {
-      Serial.println("MAX30100 ---> FAILED");
-      Serial.println("MAX30100 ---> FAILED");
+      debugPrintln("MAX30100 ---> FAILED");
+      debugPrintln("MAX30100 ---> FAILED");
     }
+    setRGBColor(0, 255, 255); // RED
+    while (true);
   }
   pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
   delay(100);
@@ -161,7 +245,7 @@ void setup() {
   // MQ135
   MQ135.setRegressionMethod(1);
   MQ135.init();
-  Serial.print("Calibrating, please wait...");
+  debugPrint("Calibrating, please wait...");
   float r0Values[MQ135_CALIBRATION_SAMPLES];
   for (int i = 0; i < MQ135_CALIBRATION_SAMPLES; i++) 
   {
@@ -174,11 +258,18 @@ void setup() {
   std::sort(r0Values, r0Values + MQ135_CALIBRATION_SAMPLES);
   float medianR0 = r0Values[MQ135_CALIBRATION_SAMPLES / 2];
   MQ135.setR0(medianR0);
-  Serial.print("Calibration done. R0 = ");
-  Serial.println(medianR0);
+  debugPrint("Calibration done. R0 = ");
+  debugPrintln(medianR0);
 
-  if(isinf(medianR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-  if(medianR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
+  if(isinf(medianR0)) 
+  {
+    setRGBColor(0, 255, 255); // RED
+    debugPrintln("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);
+  }
+  if(medianR0 == 0){
+    setRGBColor(0, 255, 255); // RED
+    debugPrintln("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);
+  }
 
   // DS18B20
   sensors.begin();
@@ -194,16 +285,17 @@ void setup() {
   xTaskCreate(ecgTask, "ECG Task", 2048, NULL, 3, NULL);
   xTaskCreate(hrTask, "HR Task", 4096, NULL, 3, NULL);
   xTaskCreate(hrPushTask, "HR Push Task", 2048, NULL, 2, NULL);
+  //xTaskCreate(sendECGTask, "Send ECG Data Task", 2048, NULL, 2, NULL);
   xTaskCreate(bodyTemperatureTask, "Body Temperature Task", 2048, NULL, 1, NULL);
   xTaskCreate(roomTempNHumTask, "Room Temperature n Humidity Task", 2048, NULL, 1, NULL);
   xTaskCreate(airQualityTask, "Air Quality Task", 2048, NULL, 1, NULL);
   xTaskCreate(processTask, "Process Task", 2048, NULL, 1, NULL);
-  xTaskCreate(debugPrintTask, "Debug Task", 2048, NULL, 1, NULL);
+  //xTaskCreate(debugPrintTask, "Debug Task", 2048, NULL, 1, NULL);
 }
 
 void ecgTask(void *parameter) {
   while (true) {
-    float ecgData = random(60, 100); // random number for now
+    float ecgData = random(60,100); // random number for now
     ecgBuffer.push(ecgData);
     vTaskDelay(pdMS_TO_TICKS(ECG_SAMPLE_RATE)); // sample rate delay
   }
@@ -220,9 +312,25 @@ void hrPushTask(void *parameter) {
   while (true) {
     float smoothedHeartRate = butterworthFilter(pox.getHeartRate());
     Serial.print("Filtered Heart Rate: ");
-    Serial.println(smoothedHeartRate);
+    Serial.println(smoothedHeartRate); Serial.println();
+    Serial.print("Normal Heart Rate: ");
+    Serial.println(pox.getHeartRate());
 
     hrBuffer.push(smoothedHeartRate);
+
+    if (smoothedHeartRate > 150)
+    {
+      digitalWrite(BUZZER_PIN, HIGH);
+    } else { digitalWrite(BUZZER_PIN, LOW); }
+
+    SensorData hrSensorData = {"<HR>|SmoothedHeartRate|", smoothedHeartRate};
+    esp_err_t result = esp_now_send(receiverMACAddress, (uint8_t *)&hrSensorData, sizeof(hrSensorData));
+    if (result == ESP_OK) {
+      debugPrint("Sent Smoothed Heart Rate: ");
+      debugPrintln(smoothedHeartRate);
+    } else {
+      debugPrintln("Error sending smoothed heart rate data");
+    }
 
     vTaskDelay(pdMS_TO_TICKS(HR_PUSH_SAMPLE_RATE)); // sample rate delay
   }
@@ -245,7 +353,14 @@ void roomTempNHumTask(void *parameter) {
 void bodyTemperatureTask(void *parameter) {
   while (true) {
     sensors.requestTemperatures();
-    SensorData tempData = {"<Body>|Temperature|", sensors.getTempCByIndex(0)};
+    float cTemp = sensors.getTempCByIndex(0);
+    SensorData tempData = {"<Body>|Temperature|", cTemp};
+
+    if (cTemp > 39 || cTemp < 32)
+    {
+      setRGBColor(0, 255, 0); // MAGENTA
+    } else { setRGBColor(255, 0, 255); }
+
     xQueueSend(sensorQueue, &tempData, portMAX_DELAY);
     vTaskDelay(pdMS_TO_TICKS(TEMP_SAMPLE_RATE)); // sample rate delay
   }
@@ -293,16 +408,43 @@ void processTask(void *parameter) {
   SensorData receivedData;
   while (true) {
     if (xQueueReceive(sensorQueue, &receivedData, portMAX_DELAY)) { // if theres data in our queue
-      Serial.print(receivedData.sensorType);
-      Serial.print(": ");
-      Serial.println(receivedData.value);
+      esp_err_t result = esp_now_send(receiverMACAddress, (uint8_t *)&receivedData, sizeof(SensorData));
+      if (result == ESP_OK) {
+        debugPrint("Sent: ");
+        debugPrint(receivedData.sensorType);
+        debugPrint(": ");
+        debugPrintln(receivedData.value);
+      } else {
+        debugPrintln("Error sending "); debugPrint(receivedData.sensorType);
+      }
     }
   }
 }
 
+void sendECGTask(void *parameter) {
+  while (true) {
+    if (ecgBuffer.count == ECG_BUFFER_SIZE) {
+      for (int i = 0; i < ECG_BUFFER_SIZE; i++) {
+        float value = ecgBuffer.pop();
+        SensorData ecgSensorData = {"<ECG>|Data Batch|", value};
+
+        esp_err_t result = esp_now_send(receiverMACAddress, (uint8_t *)&ecgSensorData, sizeof(SensorData));
+        if (result == ESP_OK) {
+          debugPrint("Sent ECG Data: ");
+          debugPrintln(value);
+        } else {
+          debugPrintln("Error sending ECG data");
+        }
+      }
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(ECG_DATA_TX_RATE));
+  }
+}
+
+
 void debugPrintTask(void *parameter) {
   while (true) {
-    // Print ECG Buffer
     Serial.print("ECG Buffer: [");
     for (int i = 0; i < ecgBuffer.count; i++) {
       Serial.print(ecgBuffer.buffer[(ecgBuffer.tail + i) % ecgBuffer.capacity]);
@@ -312,7 +454,7 @@ void debugPrintTask(void *parameter) {
     }
     Serial.println("]");
 
-    // Print Heart Rate Buffer
+    // Print Heart Rate Buffer // not important
     Serial.print("Heart Rate Buffer: [");
     for (int i = 0; i < hrBuffer.count; i++) {
       Serial.print(hrBuffer.buffer[(hrBuffer.tail + i) % hrBuffer.capacity]);
